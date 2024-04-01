28746a2b758bd236a8e82ffdc2adb550ce02e035
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-04-01 05:19:24 +0200

    Add data generation

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: ascii -*-
 
-import os, inspect
+import os, time, inspect
+import random
 import wsgif
 
 THIS_DIR = os.path.abspath(os.path.dirname(inspect.getfile(lambda: None)))
+
+RANDOM = random.SystemRandom()
+
+class NumberSupply:
+    def __init__(self):
+        self.current = [None, None, None, None]
+
+    def generate_value(self, index):
+        if index % 60 == 0:
+            return '31337'
+        else:
+            return format(RANDOM.randrange(100000), '05')
+
+    def update_values(self, now):
+        next_index = int((now + 2) / 5) * 5
+
+        if self.current[0] == next_index - 10:
+            self.current[0] += 5
+            self.current[1] = self.current[2]
+            self.current[2] = self.generate_value(next_index)
+        elif self.current[0] is None or self.current[0] < next_index - 10:
+            self.current[0] = next_index - 5
+            self.current[1] = self.generate_value(next_index - 5)
+            self.current[2] = self.generate_value(next_index)
+        else:
+            return
+
+        self.current[3] = self.current[0] + 8
+
+    def get_value(self, index, now=None):
+        if now is None:
+            now = time.time()
+        if self.current[3] is None or now >= self.current[3]:
+            self.update_values(now)
+
+        base_index = self.current[0]
+        if index == base_index:
+            return (200, self.current[1])
+        elif index == base_index + 5:
+            return (200, self.current[2])
+        elif index < base_index and index % 5 == 0:
+            return (410, '410 Gone')
+        else:
+            return (404, '404 Not Found')
+
+THE_NUMBERS = NumberSupply()
 
 route = wsgif.RouteBuilder()
 
